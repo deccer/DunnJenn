88dbda7c352629b1ef46fcc4b4213770c6c3ac2d
deccer <decccer@outlook.com>
2021-03-27 23:05:47 +0400

    Fix naming

--- src/DunnJenn/ChunkSaver.cs
+++ src/DunnJenn/ChunkSaver.cs
@@ -6,17 +6,17 @@ namespace DunnJenn
     {
         public void SaveChunk(Chunk chunk, string filePath)
         {
-            var planetId = chunk.MapId.ToString("N");
-            var planetDirectory = Path.GetDirectoryName(filePath);
-            if (!Directory.Exists(planetDirectory))
+            var mapId = chunk.MapId.ToString("N");
+            var mapDirectory = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(mapDirectory))
             {
-                Directory.CreateDirectory(planetDirectory);
+                Directory.CreateDirectory(mapDirectory);
             }
 
             using var fileStream = File.Create(filePath, 8192, FileOptions.WriteThrough);
             using var writer = new BinaryWriter(fileStream);
 
-            writer.Write((string) planetId);
+            writer.Write((string) mapId);
             writer.Write(chunk.Position.X);
             writer.Write(chunk.Position.Y);
             for (var i = 0; i < chunk.Tiles.Length; i++)
